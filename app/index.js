ba277932d88d8169b00cf2507b569b8b12375a26
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2023-06-28 23:39:32 -0400

    started setup
    dunno if it's 100% complete yet but here it is

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'; // views are divs and text a p tags
+
+const Home = () => {
+
+    return (
+        <View>
+            <Text> TEST WOOT </Text>
+        </View>
+    )
+    
+}
+
+
+export default Home;
