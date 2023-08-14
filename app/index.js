cad4fd323f78ee69b0212ea9430427a6043e4d34
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2023-08-14 16:02:29 -0400

    added basic assets and components
    sort of just went off the tutorial a bit and then added an asset or two from the figma, will get rid of folders we don't need later

--- app/index.js
+++ app/index.js
@@ -1,98 +1,17 @@
-import React from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+
+import { useState } from 'react';
+import { View, Text, SafeAreaView } from 'react-native'; // views are divs and text a p tags
+import { COLORS, icons, images, SIZES } from '../constants';
 
 const Home = () => {
-    const data = [
-        {
-            "id" : "1", 
-            "description" : "Pizza", 
-            "location": "JCC 160", 
-            "timePosted": "3 mins ago"
-        }, 
-        { 
-            "id" : "2", 
-            "description" : "Sandwiches", 
-            "location" : "SEC lobby", 
-            "timePosted" : "5 mins ago"
-        }
-    ];
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.header}>Free Food Feed</Text>
-            
-            {/* Placeholder for Search */}
-            <View style={styles.searchContainer}>
-                <Text>Search...</Text>
-            </View>
+        <SafeAreaView>
+            <Text> TEST WOOT </Text>
+        </SafeAreaView>
+    )
 
-            {/* Placeholder for Filters */}
-            <View style={styles.filterContainer}>
-                <Text>Filters...</Text>
-            </View>
-
-            {/* Live Feed */}
-            <FlatList
-                data={data}
-                renderItem={({ item }) => (
-                    <View style={styles.feedItem}>
-                        <Text style={styles.itemDescription}>{item.description}</Text>
-                        <Text style={styles.itemLocation}>{item.location}</Text>
-                        <Text style={styles.itemTime}>{item.timePosted}</Text>
-                        <Button title="I'm coming!" onPress={() => alert('Alerted the poster!')} />
-                    </View>
-                )}
-                keyExtractor={item => item.id}
-            />
-        </View>
-    );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 15,
-        backgroundColor: '#EEE1D5',
-    },
-    header: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 15,
-    },
-    searchContainer: {
-        marginBottom: 10,
-    },
-    filterContainer: {
-        marginBottom: 20,
-    },
-    feedItem: {
-        padding: 10,
-        backgroundColor: '#FFF',
-        borderRadius: 5,
-        marginBottom: 15,
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
-    itemDescription: {
-        fontSize: 16,
-        marginBottom: 5,
-    },
-    itemLocation: {
-        fontSize: 14,
-        color: '#666',
-        marginBottom: 5,
-    },
-    itemTime: {
-        fontSize: 12,
-        color: '#888',
-        marginBottom: 10,
-    }
-});
 
 export default Home;
