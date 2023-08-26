d31261f01b5a558d19ff4f8577f4a1ab5ca97ba7
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2023-08-26 11:04:02 -0400

    auth writing
    still in progress just copying over from the docs rn

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Stack } from 'expo-router';
+import { Stack, router } from 'expo-router';
 
 const Layout = () => {
     return (
---
+++ app/context/auth.tsx
@@ -0,0 +1,46 @@
+import { router, useSegments } from 'expo-router';
+import React from 'react';
+
+const AuthContext = React.createContext(null); 
+
+export function useAuth() {
+    return React.useContext(AuthContext);
+}
+
+function useProtectedRoute(user) {
+    const segments = useSegments(); // useSegments returns the current in-file 'url'
+
+    React.useEffect(() => {
+        const inAuthGroup = segments[0] === '(auth)'; // checks if current url is in (auth)
+
+        // if user not signed in AND not looking at a login page,
+        if(!user && !inAuthGroup) {
+            // redirect them to the login page
+            router.replace('/login');
+        } else if (user && inAuthGroup) {
+            router.replace('/'); // stay on apge
+        }
+
+    }, [user, segments]); // run function whenever user or segments change
+}
+
+
+// function to 
+export function Provider(props) {
+    const [user, setAuth] = React.useState(null);
+
+    useProtectedRoute(user);
+
+    return (
+        <AuthContext.Provider
+            value = { {
+                signIn: () => setAuth({}),
+                signOut: () => setAuth(null),
+                user,
+            }}>
+            {props.children}
+        </AuthContext.Provider>
+
+    )
+    
+}
