b572b6c8a48ca2c1829668a7f53d65ee3588897a
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2023-10-30 19:18:46 -0400

    added text/description input

--- app/(tabs)/post/index.tsx
+++ app/(tabs)/post/index.tsx
@@ -1,23 +1,56 @@
-import React from "react";
-import { View, Text, SafeAreaView, Image } from "react-native"; // views are divs and text a p tags
-import { ScrollView } from "react-native-gesture-handler";
+import React, { useContext, useState } from "react";
+import { View, Text, SafeAreaView, Image, TextInput } from "react-native"; // views are divs and text a p tags
 import { useAuth } from "../../../context/auth";
 import { globalStyles } from "../../../components/global";
 import { Link } from "expo-router";
+import { PostContext } from "../../../context/postContext";
 
 const Post = () => {
-  const { signOut } = useAuth();
-  return (
-    <SafeAreaView style={globalStyles.container}>
-      <View style={{ backgroundColor: "white" }}></View>
-      <Text> Make a post! </Text>
-      <View style={{ alignItems: "center", justifyContent: "center" }}>
-        <Link href="/post/camera">
-          <Text>This is the camera!</Text>
-        </Link>
-      </View>
-    </SafeAreaView>
-  );
+	const { signOut } = useAuth();
+	const { postData, updatePostData } = useContext(PostContext);
+
+	const [title, setTitle] = useState("");
+
+	const [descr, setDescr] = React.useState("");
+
+	const handleUpdateData = () => {
+		updatePostData({ title: title, description: descr }); // Update multiple values
+	};
+	return (
+		<SafeAreaView
+			style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+		>
+			<View style={{ backgroundColor: "white" }}></View>
+			<Text> Make a post! </Text>
+			<TextInput
+				placeholder="Add Food Name"
+				value={title}
+				onChangeText={(text) => {
+					setTitle(text);
+					handleUpdateData();
+				}}
+			></TextInput>
+			<TextInput
+				placeholder="Write a description for each food item. Please include the name of the restaurant its from if you can！"
+				multiline
+				numberOfLines={4}
+				maxLength={40}
+				onChangeText={(text) => {
+					setDescr(text);
+					handleUpdateData();
+				}}
+				value={descr}
+			></TextInput>
+
+			<Text> Title {postData.title}</Text>
+			<Text> Description {postData.description}</Text>
+			<View style={{ alignItems: "center", justifyContent: "center" }}>
+				<Link href="/post/camera">
+					<Text>This is the camera!</Text>
+				</Link>
+			</View>
+		</SafeAreaView>
+	);
 };
 
 export default Post;
--- app/(tabs)/post/tags.tsx
+++ app/(tabs)/post/tags.tsx
@@ -1,15 +1,37 @@
-import { View, Text, SafeAreaView, Button } from "react-native";
+import {
+	View,
+	Text,
+	SafeAreaView,
+	Button,
+	TouchableOpacity,
+} from "react-native";
 import { Link } from "expo-router";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import BackButton from "../../../components/common/BackButton";
 import { PostContext } from "../../../context/postContext";
 // declare object that's only the tags here
 const tags = () => {
 	const { postData, updatePostData } = useContext(PostContext);
 
+	// declares default tags
+	const defaultTags = {
+		perishable: true,
+		allergens: [],
+		diet: [],
+	};
+
+	const [perishable, setPerishable] = useState<boolean>(
+		defaultTags.perishable
+	);
+	const [allergens, setAllergens] = useState<string[]>(defaultTags.allergens);
+	const [diet, setDiet] = useState<string[]>(defaultTags.diet);
+	const isSelected = true;
+
 	const hi = true;
 	const handleUpdateData = () => {
-		updatePostData({ title: "food1", description: "yummy food" }); // Update multiple values
+		updatePostData({
+			tags: { perishable: perishable, allergens: allergens, diet: diet },
+		}); // Update multiple values
 		console.log("updating values");
 	};
 
@@ -18,12 +40,47 @@ const tags = () => {
 			style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
 		>
 			<BackButton />
-			<Button title="Update Data" onPress={handleUpdateData} />
+
 			<Link href="/post/reviewpost">Review Post</Link>
 			<Text>Title: {postData.title}</Text>
+			<FoodTypeSelect onPress={setPerishable} />
+			<Text> Perishable: {perishable ? "yes" : "no"}</Text>
 			<Text>Description: {postData.description}</Text>
 		</SafeAreaView>
 	);
+};
+
+const FoodTypeSelect = (props: { onPress?: any }) => {
+	const [leftIsSelected, setLeftSelected] = useState<boolean>(true);
+
+	return (
+		<View style={{ backgroundColor: "#EDA76E", width: 100, height: 50 }}>
+			<TouchableOpacity
+				onPress={() => {
+					setLeftSelected(true);
+					props.onPress(true);
+				}}
+				style={{ backgroundColor: leftIsSelected ? "red" : "white" }}
+			>
+				<Text>Perishable</Text>
+			</TouchableOpacity>
+
+			<TouchableOpacity
+				onPress={() => {
+					setLeftSelected(false);
+					props.onPress(false);
+				}}
+				style={{ backgroundColor: leftIsSelected ? "white" : "red" }}
+			>
+				<Text>Non-Perishable</Text>
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+const tagBox = ({ label, checked, onChange }) => {
+	const [isSelected, setSelected] = useState<boolean>(true);
+	<TouchableOpacity></TouchableOpacity>;
 };
 
 export function exportTags() {}
