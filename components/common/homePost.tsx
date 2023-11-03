aacddd33853750fee3409a51870063b83c3af2a4
vle04 <ngocvinale@gmail.com>
2023-11-03 11:22:44 -0400

    Co-authored-by: Johnny Tan <johnny-t06@users.noreply.github.com>

--- components/common/homePost.tsx
+++ components/common/homePost.tsx
@@ -4,16 +4,15 @@ import { useState } from "react";
 import styled from "styled-components/native";
 import * as postType from "../../context/postContext"
 import { StyleSheetContext } from "styled-components";
-
-
-
+import { faBookmark } from '@fortawesome/free-solid-svg-icons/faBookmark'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 
 const HomePost = (props) => {
     // const [postData, setPostData] = useState<typeof dummyData>();
     const dummyData: postType.postType = {
 		title: "testtitle",
 		description: "testDes",
-		imageURIs: [""],
+		imageURIs: ['../../assets/images/the-pizza-box.jpeg'],
 		tags: {
 			perishable: true,
 			allergens: ["peanuts"],
@@ -28,7 +27,22 @@ const HomePost = (props) => {
 
     return (
 		<View style = {styles.mainbox}>
-			<Text>Testing from component {dummyData.title}</Text>
+		
+			<View style = {styles.imagebox}> 
+			<Image
+				source = {{
+					uri: dummyData.imageURIs[0]
+				}}
+
+				style = {styles.image}
+			/>
+			</View>
+			<View style = {styles.description}>
+				<View style = {styles.title}>
+					Pizzas
+					<FontAwesomeIcon icon = {faBookmark} style = {styles.bookmark}/>
+				</View>
+			</View>
 		</View>
 		
 	);
@@ -42,10 +56,38 @@ const styles = StyleSheet.create({
 		borderRadius: 20,
 		elevation: 5,
 		shadowColor: '#000',
-		shadowOffset: { width: 0, height: 3 },
-		shadowOpacity: 0.8,
-		shadowRadius: 3,
-	}
+		shadowOffset: { width: 0, height: 4 },
+		shadowOpacity: 0.2,
+		shadowRadius: 4,
+		paddingHorizontal: 15,
+		paddingVertical: 15,
+
+		flexDirection: "row",
+	},
+	imagebox: {
+		flex: 1,
+		width: 85,
+		marginRight: 10,
+	}, 
+	image: {
+		flex: 1,
+		borderRadius: 15,
+	},
+	description: {
+		flex: 3,
+		marginLeft: 10,
+		flexDirection: "column",
+	},
+	title: {
+		height: 25,
+		flexDirection: "row",
+		justifyContent: "flex-end",
+		flex: 1,
+	},
+	bookmark: {
+		flex: 1,
+		width: 10,
+	},
 })
 
 export default HomePost;
