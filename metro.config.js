2e17d0ba6d6a98b76c0348c63f6ec35ef0f5b151
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2024-02-04 21:17:59 -0500

    quick hotfix for web building
    notes - firebase native storage disabled for web (although it still seems to persist? not sure)
    added mjs to metro config

--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,5 @@ const { getDefaultConfig } = require('@expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.sourceExts.push('cjs');
-
+defaultConfig.resolver.sourceExts = [...defaultConfig.resolver.sourceExts, "mjs", "cjs"];
 module.exports = defaultConfig;
