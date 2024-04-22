03401e7905adbd9029b0ac94e77f58fc68393713
Johnny Tan <jonnytan0818@gmail.com>
2024-04-22 15:40:27 -0400

    removed wrong router description

--- backend/routes/notifyRoutes.ts
+++ backend/routes/notifyRoutes.ts
@@ -1,15 +1,6 @@
 import express from "express";
 import * as notify from "../controllers/notifyControllers";
-/*
- * postRoutes.ts
- *
- * purpose: defines and exports the routes for interacting with the post
- * 			database.
- *
- * takes in controller functions to execute at those endpoints, defined in
- * controllers/postControllers
- *
- */
+
 const notifyRouter = express.Router();
 
 notifyRouter.post("/api/notify", notify.notifyUsers);
