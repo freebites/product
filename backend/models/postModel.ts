f14077921834302fb7611d7e1babbd10533eb1ce
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2024-05-22 20:39:55 -0400

    updated post schema

--- backend/models/postModel.ts
+++ backend/models/postModel.ts
@@ -1,48 +1,50 @@
 import mongoose from "mongoose";
 
-// const allergen = new mongoose.Schema({
-// 	allergen: String,
-// });
+const allergen = new mongoose.Schema({
+  allergen: String,
+});
 
-// const diet = new mongoose.Schema({
-// 	diet: String,
-// });
+const diet = new mongoose.Schema({
+  diet: String,
+});
 
 const tags = new mongoose.Schema({
-	perishable: Boolean,
-	allergens: [String],
-	diet: [String],
+  perishable: Boolean,
+  allergens: [allergen],
+  diet: [diet],
 });
 
 const comment = new mongoose.Schema(
-	{
-		username: String,
-		body: String,
-		timestamp: Date, // may need to be changed to string
-	},
+  {
+    username: String,
+    body: String,
+    timestamp: Date, // may need to be changed to string
+  }
+  // disables mongoDB id creation, we can re-enable if we
+  // run into issues w/ rendering comments (ensure unique keys)
 );
 
 const Location = new mongoose.Schema({
-	place_id: String,
-	location: {
-		type: { type: String },
-		coordinates: [Number],
-	},
+  place_id: String,
+  location: {
+    type: { type: String },
+    coordinates: [Number],
+  },
 });
 // const imageURI = new mongoose.Schema({ URI: String });
 
 // Define a schema for the "items" collection
 const itemSchema = new mongoose.Schema({
-	// _id: mongoose.Schema.Types.ObjectID,
-	title: String,
-	description: String,
-	imageURIs: [String],
-	tag: tags,
-	location: Location,
-	comments: [comment],
-	post_id: String,
-	room: String,
-	postTime: Date,
+  // _id: mongoose.Schema.Types.ObjectID,
+  title: String,
+  description: String,
+  imageURIs: [String],
+  tag: tags,
+  location: Location,
+  comments: [comment],
+  post_id: String,
+  room: String,
+  postTime: Date,
 });
 
 // Create a model for the "items" collection
