b79df3b84dd2b2d2c201fe25a5be56ae83980384
Johnny Tan <jonnytan0818@gmail.com>
2024-05-26 09:01:54 -0700

    post popup redesign to new figma

--- src/components/home/DisplayTags.tsx
+++ src/components/home/DisplayTags.tsx
@@ -1,56 +1,72 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, Pressable, StyleSheet } from "react-native";
+import { tags } from "../../../types/PostTypes";
+import { CardTag } from "../common/CardTag";
 const vegetarian = require("../../assets/icons/freebites/vegetarian.png");
 const perishableImg = require("../../assets/icons/freebites/perishable.png");
 const nonperishable = require("../../assets/icons/freebites/nonperishable.png");
 const msg = require("../../assets/icons/freebites/msg.png");
 const lactose = require("../../assets/icons/freebites/lactose.png");
 
-const DisplayTags = (props) => {
-  const { tag } = props;
+interface DisplayTagsProps {
+  tags: tags;
+}
+const DisplayTags = (props: DisplayTagsProps) => {
+  const { tags } = props;
 
   // Destructure perishable, allergens, and diet from the tag object
-  const { perishable, allergens, diet } = tag;
+  const { perishable, allergens, diet } = tags;
 
-  let perishableElement = null;
-  let allergenElements = null;
-  let dietElements = null;
+  // let perishableElement = null;
+  // let allergenElements = null;
+  // let dietElements = null;
 
-  if (perishable) {
-    perishableElement = <Image source={perishableImg} />;
-  }
+  // if (perishable) {
+  //   perishableElement = <Image source={perishableImg} />;
+  // }
 
-  allergenElements = allergens.map((allergen, index) => {
-    switch (allergen) {
-      case "peanut":
-        return <Text key={index}>Peanut</Text>;
-      case "dairy":
-        return <Image key={index} source={lactose} />;
-      case "tree nut":
-        return <Text key={index}>Tree nut</Text>;
-      default:
-        return null;
-    }
-  });
+  // allergenElements = allergens.map((allergen, index) => {
+  //   switch (allergen) {
+  //     case "peanut":
+  //       return <Text key={index}>Peanut</Text>;
+  //     case "dairy":
+  //       return <Image key={index} source={lactose} />;
+  //     case "tree nut":
+  //       return <Text key={index}>Tree nut</Text>;
+  //     default:
+  //       return null;
+  //   }
+  // });
 
-  dietElements = diet.map((dietType, index) => {
-    switch (dietType) {
-      case "vegetarian":
-        return <Image key={index} source={vegetarian} />;
-      case "vegan":
-        return <Text key={index}>Vegan</Text>;
-      default:
-        return null;
-    }
-  });
+  // dietElements = diet.map((dietType, index) => {
+  //   switch (dietType) {
+  //     case "vegetarian":
+  //       return <Image key={index} source={vegetarian} />;
+  //     case "vegan":
+  //       return <Text key={index}>Vegan</Text>;
+  //     default:
+  //       return null;
+  //   }
+  // });
 
   return (
-    <View>
-      {perishableElement}
-      {allergenElements}
-      {dietElements}
+    <View style={styles.container}>
+      <CardTag tag={perishable} />
+      {allergens.map((allergen, index) => (
+        <CardTag key={index} tag={allergen} />
+      ))}
+
+      {diet.map((eachdiet, index) => (
+        <CardTag key={index} tag={eachdiet} />
+      ))}
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+});
 export default DisplayTags;
