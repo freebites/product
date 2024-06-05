e93d5493b0def400f8162fee69997cd4ced4db25
Owen Prendergast <owenprendergast8@gmail.com>
2024-06-05 16:10:58 -0700

    fix for hook error

--- src/app/(tabs)/profile/history.tsx
+++ src/app/(tabs)/profile/history.tsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet, ScrollView } from "react-native";
 import React, { useState, useContext, useEffect } from "react";
 import { globalStyles } from "../../../components/global";
 import Header from "../../../components/common/Header";
@@ -9,101 +9,48 @@ import { postType } from "../../../../types/PostTypes";
 import { router } from "expo-router";
 import HomePost from "../../../components/home/HomePost";
 import { AppContext } from "../../../context/appContext";
-// import { useState } from "react";
-
-/*
-	TODO: 
-		- Currently the localParams in this page is the UID
-		- For backend, each User should have a [postIDs] 
-		- Pass in the user's postIDs instead of the whole user object
-		- Display each Post
-*/
+import { RefreshControl } from "react-native-gesture-handler";
 
 // use local search parameters
 const History = () => {
   validateRoutePerms();
+  const { user } = useAuth();
   const [AllPosts, setPosts] = useState<postType[]>([]);
-  const {
-    filters,
-    location,
-    setLocation,
-    userToFilter,
-    setUserToFilter,
-    sort,
-  } = useContext(AppContext);
+  const [refreshing, setRefreshing] = useState(true);
 
   const getYourPosts = async () => {
-    // const [AllPosts, setPosts] = useState([]);
-    const { user } = useAuth();
     let postData;
     postData = await getAllPosts();
-    console.log(postData.length);
+
     postData = postData.filter((eachPost: postType) => {
-      // console.log(eachPost.postedBy);
-      // console.log(user.uid);
       return eachPost.postedBy == user.uid;
     });
-    console.log(postData.length);
 
-    postData.map((eachPost: postType) => {
-      console.log("Found a post that you posted in post history");
-      // console.log(user.uid == eachPost.postedBy);
-    });
-
-    // setPosts(postData);
     setPosts(postData);
   };
 
-  let posts: postType[] = [];
-
   useEffect(() => {
-    // async function
-    // fetchData();
-    // setRefreshing(true);
     getYourPosts();
-  }, [userToFilter, filters]);
-  // const { user } = useAuth();
-  // let posts = getYourPosts();
-  // let postData;
-  // postData = getWithFilter({
-  //   diet: [],
-  //   latitude: 0,
-  //   longitude: 0,
-  //   userID: "",
-  //   sort: 0,
-  // });
+  }, []);
 
   return (
     <SafeAreaView style={globalStyles.container}>
       <Header text="History" />
-
-      <View
-        style={{
-          margin: "5%",
-          alignItems: "center",
-          justifyContent: "center",
-          flex: 1,
-        }}
-      >
-        <Text
-          style={{
-            fontWeight: "bold",
-            fontSize: 24,
-            color: "#505A4E",
-            textShadowRadius: 1,
-            textShadowColor: "black",
-            paddingBottom: 12,
-          }}
+      {AllPosts.length > 0 ? (
+        <ScrollView
+          contentContainerStyle={styles.postContainer}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={getYourPosts} />
+          }
         >
           {AllPosts.map((eachPost: postType) => {
-            // here?
             return (
               <HomePost
                 style={styles.postCard}
                 key={eachPost._id}
                 post={eachPost}
-                setRefreshing={() => null}
-                fetchData={() => null}
+                setRefreshing={setRefreshing}
+                fetchData={getYourPosts}
                 onPress={() =>
                   router.push({
                     pathname: "/postPopUp",
@@ -112,15 +59,37 @@ const History = () => {
                   })
                 }
               />
-              //<Text>{JSON.stringify(eachPost)}</Text>
             );
           })}
-          No history yet
-        </Text>
-        <Text style={{ textAlign: "center", color: "#505A4E", opacity: 0.57 }}>
-          Try making a post by clicking on the + button on the homepage!
-        </Text>
-      </View>
+        </ScrollView>
+      ) : (
+        <View
+          style={{
+            margin: "5%",
+            alignItems: "center",
+            justifyContent: "center",
+            flex: 1,
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 24,
+              color: "#505A4E",
+              textShadowRadius: 1,
+              textShadowColor: "black",
+              paddingBottom: 12,
+            }}
+          >
+            No history yet
+          </Text>
+          <Text
+            style={{ textAlign: "center", color: "#505A4E", opacity: 0.57 }}
+          >
+            Try making a post by clicking on the + button on the homepage!
+          </Text>
+        </View>
+      )}
     </SafeAreaView>
   );
 };
--- src/components/home/DeleteModal.tsx
+++ src/components/home/DeleteModal.tsx
@@ -12,7 +12,6 @@ import { Icon } from "react-native-elements";
 
 import deleteOne from "../../../api/posts/delete";
 import fetchData from "../../app/(tabs)/home/index";
-// import forceRefresh from "../../app/(tabs)/home/index";
 import { router } from "expo-router";
 
 interface DeleteButtonProps {
@@ -26,77 +25,70 @@ const DeleteModal = (props: DeleteButtonProps) => {
   const [modalVisible, setModalVisible] = useState(false);
   const { postID, userPost, setRefreshing, fetchData } = props;
 
-  if (userPost) {
-    return (
-      <View style={styles.trashIcon}>
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          // coverScreen={true}
-          // hasBackdrop={isBackdrop}
-          onRequestClose={() => {
-            Alert.alert("Modal has been closed.");
-            setModalVisible(!modalVisible);
-          }}
-        >
-          <View style={styles.bottomView}>
-            {/* <img src={logo} alt="carrot logo" /> */}
-            <Image
-              source={require("../../assets/icons/freebites/FreeBitesLogoSmall.png")}
-              style={styles.imgStyle}
-            />
-            <View style={styles.modalView}>
-              <Text style={styles.modalText}>
-                Are you sure you want to delete this post?
-              </Text>
-              {/* <hr style={styles.hr}></hr> */}
-              <View style={styles.hr} />
-              <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={async () => {
-                  setModalVisible(!modalVisible);
-                  await deleteOne(postID);
-                  // fetchData();
-                  // setRefreshing(true);
-                  // router.dismissAll();
-                  router.push("/home");
-                  // forceRefresh();
-                }}
-              >
-                <Text style={styles.textStyle}>Yes!</Text>
-              </Pressable>
-              {/* <hr style={styles.hr}></hr> */}
-              <View style={styles.hr} />
-              <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={() => setModalVisible(!modalVisible)}
-              >
-                <Text style={styles.textStyle}>No Oops!</Text>
-              </Pressable>
+  return (
+    <View style={styles.trashIcon}>
+      {userPost ? (
+        <View>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            // coverScreen={true}
+            // hasBackdrop={isBackdrop}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.bottomView}>
+              <Image
+                source={require("../../assets/icons/freebites/FreeBitesLogoSmall.png")}
+                style={styles.imgStyle}
+              />
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>
+                  Are you sure you want to delete this post?
+                </Text>
+                <View style={styles.hr} />
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={async () => {
+                    setModalVisible(!modalVisible);
+                    await deleteOne(postID);
+                    fetchData();
+                    setRefreshing(true);
+                    // router.push("/home");
+                  }}
+                >
+                  <Text style={styles.textStyle}>Yes!</Text>
+                </Pressable>
+                <View style={styles.hr} />
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={styles.textStyle}>No Oops!</Text>
+                </Pressable>
+              </View>
+              <View style={styles.cancelButton}>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={styles.textStyle}>Cancel</Text>
+                </Pressable>
+              </View>
             </View>
-            <View style={styles.cancelButton}>
-              <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={() => setModalVisible(!modalVisible)}
-              >
-                <Text style={styles.textStyle}>Cancel</Text>
-              </Pressable>
-            </View>
-          </View>
-        </Modal>
-        <Pressable
-          // style={[styles.button, styles.buttonOpen]}
-          onPress={() => setModalVisible(true)}
-        >
-          {/* <Text style={styles.textStyle}>Show Modal</Text> */}
-          <Icon type={"entypo"} name={"trash"}></Icon>
-        </Pressable>
-      </View>
-    );
-  } else {
-    return null;
-  }
+          </Modal>
+          <Pressable onPress={() => setModalVisible(true)}>
+            <Icon type={"entypo"} name={"trash"}></Icon>
+          </Pressable>
+        </View>
+      ) : (
+        <View> </View>
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -111,9 +103,6 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 1.25,
     margin: 1,
-    // borderBottomWidth: 1,
-
-    // borderBottomColor: "gray",
   },
   imgStyle: {
     width: 45,
@@ -126,7 +115,6 @@ const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "flex-end",
     alignItems: "center",
-    // margin: "auto",
     backgroundColor: "rgba(0,0,0,0.4)",
   },
   modalView: {
@@ -157,7 +145,6 @@ const styles = StyleSheet.create({
   button: {
     borderRadius: 20,
     padding: 10,
-    // margin: 5,
     elevation: 2,
   },
   buttonOpen: {
