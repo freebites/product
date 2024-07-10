dec3a7106442252285668e6dd6aae76e1ff44d7c
Jet4stream <yotsuuye@gmail.com>
2024-07-10 11:04:19 -0700

    Updated the Report.tsx to display Report Modal

--- src/components/home/ReportModal.tsx
+++ src/components/home/ReportModal.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, Pressable, View, Image } from "react-native";
+import { StyleSheet, Text, Pressable, View, Image, KeyboardAvoidingView, TextInput } from "react-native";
 import Modal from "react-native-modal";
 import { COLORS } from "../../constants/theme";
 
@@ -10,10 +10,20 @@ interface ReportModal {
 
 export const ReportModal = (props: ReportModal) => {
   const { reportVisible, setReportVisible } = props;
+  const [text, changeText] = useState<string>("");
   const [submitted, setSubmitted] = useState<boolean>(false);
+  const [introText, setIntroText] = useState<string>(
+    "Would you like to report or share this post?"
+  );
+  const [buttonText, setButtonText] = useState<string>("Submit");
 
-  console.log("reportVisible: " + reportVisible);
-  // console.log("modalVisible: " + modalVisible);
+  const submitReport = () => {
+    setIntroText(
+      "Thank you for keeping this community safe! Our team will address this problem as soon as possible :)"
+    );
+    setButtonText("I got it!");
+  };
+
   return (
     <Modal
       animationIn={"slideInUp"}
@@ -31,9 +41,61 @@ export const ReportModal = (props: ReportModal) => {
       }}
       style={styles.modalContainer}
     >
-      <View>
-        <Text>yippeee</Text>
-      </View>
+      <KeyboardAvoidingView
+            behavior="position"
+            style={{ width: "100%" }}
+            keyboardVerticalOffset={25}
+          >
+            <Image
+              source={require("../../assets/icons/freebites/FreeBitesLogoSmall.png")}
+              style={styles.imgStyle}
+            />
+
+            <View style={styles.textContainer}>
+              <Text style={styles.text}>{introText}</Text>
+              {submitted ? (
+                <View></View>
+              ) : (
+                <TextInput
+                  style={styles.textInput}
+                  onChangeText={(newText) => {
+                    changeText(newText);
+                  }}
+                  value={text}
+                  placeholder="Please type your answer here"
+                  placeholderTextColor="#AEA9B1"
+                />
+              )}
+            </View>
+            {text !== "" ? (
+              <Pressable
+                style={styles.submitButton}
+                onPress={() => {
+                  if (!submitted) {
+                    submitReport();
+                    setSubmitted(true);
+                  } else {
+                    setReportVisible();
+                    setSubmitted(false);
+                    changeText("");
+                    setIntroText(
+                      "Would you like to report or share this post?"
+                    );
+                    setButtonText("Submit");
+                  }
+                }}
+              >
+                <Text>{buttonText}</Text>
+              </Pressable>
+            ) : (
+              <Pressable
+                style={styles.cancelButton}
+                onPress={() => setReportVisible()}
+              >
+                <Text>Cancel</Text>
+              </Pressable>
+            )}
+          </KeyboardAvoidingView>
     </Modal>
   );
 };
@@ -54,8 +116,50 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
   },
 
+  imgStyle: {
+    width: 46,
+    height: 52,
+    marginBottom: 10,
+    alignSelf: "center",
+  },
+
+  textInput: {
+    width: "90%",
+    height: 86,
+    borderWidth: 1,
+    borderColor: COLORS.gray,
+    marginVertical: 10,
+    borderRadius: 20,
+    padding: 20,
+  },
+
+  textContainer: {
+    backgroundColor: COLORS.white,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+    width: "100%",
+    height: 153,
+    paddingVertical: 10,
+  },
+
+  text: {
+    textAlign: "center",
+    width: "90%",
+  },
+
   cancelButton: {
     backgroundColor: COLORS.white,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+    width: "100%",
+    height: 43,
+    marginTop: 5,
+  },
+
+  submitButton: {
+    backgroundColor: COLORS.orange[80],
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 20,
