96266622d60003806adebcca070d564b13e51989
Jet4stream <yotsuuye@gmail.com>
2024-07-10 14:29:36 -0700

    Cleaned up the Option.tsx

--- src/components/home/OptionModel.tsx
+++ src/components/home/OptionModel.tsx
@@ -5,8 +5,6 @@ import {
   Pressable,
   View,
   Image,
-  TextInput,
-  KeyboardAvoidingView,
 } from "react-native";
 import Modal from "react-native-modal";
 import { Icon } from "react-native-elements";
@@ -17,21 +15,6 @@ import { ReportModal } from "../home/ReportModal.tsx";
 const OptionModel = () => {
   const [modalVisible, setModalVisible] = useState<boolean>(false);
   const [reportModalVisible, setReportModalVisible] = useState<boolean>(false);
-
-  //Report Model
-  const [text, changeText] = useState<string>("");
-  const [submitted, setSubmitted] = useState<boolean>(false);
-  const [introText, setIntroText] = useState<string>(
-    "Would you like to report or share this post?"
-  );
-  const [buttonText, setButtonText] = useState<string>("Submit");
-
-  const submitReport = () => {
-    setIntroText(
-      "Thank you for keeping this community safe! Our team will address this problem as soon as possible :)"
-    );
-    setButtonText("I got it!");
-  };
 
   const changeReportVisible = () => {
     setReportModalVisible(!reportModalVisible);
@@ -46,7 +29,6 @@ const OptionModel = () => {
     setReportModalVisible(false);
   };
 
-  // console.log("1: report visible in options file: " + reportModalVisible);
   return (
     <View>
       <Pressable
@@ -118,85 +100,10 @@ const OptionModel = () => {
         <View>
           <ReportModal
             reportVisible={reportModalVisible}
-            // setReportVisible={changeReportVisible}
             setReportVisible={closeReportModal}
           ></ReportModal>
         </View>
       )}
-
-      {/* <View>
-        <Modal
-          animationIn={"slideInUp"}
-          animationInTiming={400}
-          animationOut={"slideOutDown"}
-          animationOutTiming={300}
-          isVisible={modalVisible}
-          backdropTransitionOutTiming={0}
-          hasBackdrop
-          backdropOpacity={0.55}
-          coverScreen
-          onBackdropPress={() => {
-            setModalVisible(!modalVisible);
-          }}
-          style={styles.modalContainer}
-        >
-          <KeyboardAvoidingView
-            behavior="position"
-            style={{ width: "100%" }}
-            keyboardVerticalOffset={25}
-          >
-            <Image
-              source={require("../../assets/icons/freebites/FreeBitesLogoSmall.png")}
-              style={styles.imgStyle}
-            />
-
-            <View style={styles.textContainer}>
-              <Text style={styles.text}>{introText}</Text>
-              {submitted ? (
-                <View></View>
-              ) : (
-                <TextInput
-                  style={styles.textInput}
-                  onChangeText={(newText) => {
-                    changeText(newText);
-                  }}
-                  value={text}
-                  placeholder="Please type your answer here"
-                  placeholderTextColor="#AEA9B1"
-                />
-              )}
-            </View>
-            {text !== "" ? (
-              <Pressable
-                style={styles.submitButton}
-                onPress={() => {
-                  if (!submitted) {
-                    submitReport();
-                    setSubmitted(true);
-                  } else {
-                    setModalVisible(false);
-                    setSubmitted(false);
-                    changeText("");
-                    setIntroText(
-                      "Would you like to report or share this post?"
-                    );
-                    setButtonText("Submit");
-                  }
-                }}
-              >
-                <Text>{buttonText}</Text>
-              </Pressable>
-            ) : (
-              <Pressable
-                style={styles.cancelButton}
-                onPress={() => setModalVisible(false)}
-              >
-                <Text>Cancel</Text>
-              </Pressable>
-            )}
-          </KeyboardAvoidingView>
-        </Modal>
-      </View> */}
     </View>
   );
 };
@@ -214,16 +121,6 @@ const styles = StyleSheet.create({
     alignSelf: "center",
   },
 
-  textInput: {
-    width: "90%",
-    height: 86,
-    borderWidth: 1,
-    borderColor: COLORS.gray,
-    marginVertical: 10,
-    borderRadius: 20,
-    padding: 20,
-  },
-
   textContainer: {
     backgroundColor: COLORS.white,
     alignItems: "center",
@@ -232,11 +129,6 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 153,
     paddingVertical: 10,
-  },
-
-  text: {
-    textAlign: "center",
-    width: "90%",
   },
 
   optionButton: {
@@ -248,16 +140,6 @@ const styles = StyleSheet.create({
 
   cancelButton: {
     backgroundColor: COLORS.white,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 20,
-    width: "100%",
-    height: 43,
-    marginTop: 5,
-  },
-
-  submitButton: {
-    backgroundColor: COLORS.orange[80],
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 20,
